1fe0fa0838854e4d02c23aea933be9d4f4e129d2
Agil Desenvolvimento <rodrigues.tallyto@hotmail.com>
2019-04-22 12:37:58 -0400

    Adicionado a biblioteca de local storage

--- src/pages/pizzasTradicionais.js
+++ src/pages/pizzasTradicionais.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { View , Text} from 'react-native';
+import { View, Text, Button } from "react-native";
 
+import AsyncStorage from "@react-native-community/async-storage";
+
+import api from "../services/api";
 // import { Container } from './styles';
 
 export default class PizzasTradicionais extends Component {
+  state = {
+    dados: []
+  };
+
+  pegarDados = async () => {
+    const response = await api.get("/api/PizzasTradicionais");
+
+    const dados = response.data;
+
+    this.setState({ dados: dados });
+    //this.setState({ dados: dados});
+  };
+
+  componentDidMount() {}
+
   render() {
-    return <View>
-        <Text>Pizzas Tradicionais</Text>
-    </View>;
+    return (
+      <View>
+        <Text>Pizzas</Text>
+
+        {this.state.dados.map(pizzas => (
+          <View key={pizzas._id}>
+            <Text style={{ fontWeight: "bold" }}>{pizzas.title}</Text>
+            <Text>{pizzas.description}</Text>
+          </View>
+        ))}
+        <Button onPress={this.pegarDados} title="Pegar dados" />
+      </View>
+    );
   }
 }
---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import { create } from "apisauce";
+
+const api = create({
+  baseURL: "https://dois90api.cleverapps.io"
+});
+
+export default api;
